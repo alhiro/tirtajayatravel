5451e2483d14e0cddb0db5306a1f6bcec5eef29d
Alkim Hiro <aliezio86@hotmail.com>
2025-01-12 14:17:34 +0700

    update dashboard  graphic

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -41,6 +41,9 @@ export class HomeComponent implements OnInit {
   // Paket Progress Paket No Take Malang
   public listDataPackageNoTakeMalang: any;
   public progressPercentagePackageNoTakeMalang: any;
+  // Paket Progress Paket Take Malang
+  public listDataPackageTakeMalang: any;
+  public progressPercentagePackageTakeMalang: any;
   // Paket Progress Penumpang Malang
   public listDataPassengerMalang: any;
   public progressPercentagePassengerMalang: any;
@@ -54,6 +57,9 @@ export class HomeComponent implements OnInit {
   // Paket Progress Paket No Take Surabaya
   public listDataPackageNoTakeSurabaya: any;
   public progressPercentagePackageNoTakeSurabaya: any;
+  // Paket Progress Paket Take Surabaya
+  public listDataPackageTakeSurabaya: any;
+  public progressPercentagePackageTakeSurabaya: any;
   // Paket Progress Penumpang Surabaya
   public listDataPassengerSurabaya: any;
   public progressPercentagePassengerSurabaya: any;
@@ -103,8 +109,8 @@ export class HomeComponent implements OnInit {
 
         if (this.getDataPackageMalang) {
           // Progress Paket Malang
-          const paketProgressMalang = parseInt(this.getDataPackageMalang?.count_progress) ?? 0;
-          const totalPaketProgressMalang = parseInt(this.getDataPackageMalang?.count_completed) ?? 0;
+          const paketProgressMalang = parseInt(this.getDataPackageMalang?.count_progress) || 0;
+          const totalPaketProgressMalang = parseInt(this.getDataPackageMalang?.count_completed) || 0;
           this.progressPercentagePackageMalang =
             paketProgressMalang || totalPaketProgressMalang === 0
               ? 0
@@ -112,12 +118,12 @@ export class HomeComponent implements OnInit {
           this.listDataPackageMalang = {
             progress: paketProgressMalang,
             totalProgress: totalPaketProgressMalang,
-            percentage: this.progressPercentagePackageMalang ?? 0,
+            percentage: this.progressPercentagePackageMalang || 0,
           };
 
           // Progress Paket Sudah Kirim Malang
-          const paketProgressSendMalang = parseInt(this.getDataPackageMalang?.count_delivery) ?? 0;
-          const totalPaketProgressSendMalang = parseInt(this.getDataPackageMalang?.count_completed) ?? 0;
+          const paketProgressSendMalang = parseInt(this.getDataPackageMalang?.count_delivery) || 0;
+          const totalPaketProgressSendMalang = parseInt(this.getDataPackageMalang?.count_completed) || 0;
           this.progressPercentagePackageSendMalang =
             paketProgressSendMalang || totalPaketProgressSendMalang === 0
               ? 0
@@ -125,15 +131,15 @@ export class HomeComponent implements OnInit {
           this.listDataPackageSendMalang = {
             progress: paketProgressSendMalang,
             totalProgress: totalPaketProgressSendMalang,
-            percentage: this.progressPercentagePackageSendMalang ?? 0,
+            percentage: this.progressPercentagePackageSendMalang || 0,
           };
           console.log(`${paketProgressSendMalang}, ${totalPaketProgressSendMalang}`);
           console.log(this.progressPercentagePackageSendMalang);
           console.log(this.listDataPackageSendMalang);
 
           // Progress Paket Belum Diambil Malang
-          const paketProgressNoTakeMalang = parseInt(this.getDataPackageMalang?.count_nocompleted) ?? 0;
-          const totalPaketProgressNoTakeMalang = parseInt(this.getDataPackageMalang?.count_completed) ?? 0;
+          const paketProgressNoTakeMalang = parseInt(this.getDataPackageMalang?.count_nocompleted) || 0;
+          const totalPaketProgressNoTakeMalang = parseInt(this.getDataPackageMalang?.count_completed) || 0;
           this.progressPercentagePackageNoTakeMalang =
             paketProgressNoTakeMalang || totalPaketProgressNoTakeMalang === 0
               ? 0
@@ -141,7 +147,20 @@ export class HomeComponent implements OnInit {
           this.listDataPackageNoTakeMalang = {
             progress: paketProgressNoTakeMalang,
             totalProgress: totalPaketProgressNoTakeMalang,
-            percentage: this.progressPercentagePackageNoTakeMalang ?? 0,
+            percentage: this.progressPercentagePackageNoTakeMalang || 0,
+          };
+
+          // Progress Paket Sudah Diambil Malang
+          const paketProgressTakeMalang = parseInt(this.getDataPackageMalang?.count_completed) || 0;
+          const totalPaketProgressTakeMalang = parseInt(this.getDataPackageMalang?.count_completed) || 0;
+          this.progressPercentagePackageTakeMalang =
+            paketProgressTakeMalang || totalPaketProgressTakeMalang === 0
+              ? 0
+              : (totalPaketProgressTakeMalang / paketProgressTakeMalang) * 100;
+          this.listDataPackageTakeMalang = {
+            progress: paketProgressTakeMalang,
+            totalProgress: totalPaketProgressTakeMalang,
+            percentage: this.progressPercentagePackageTakeMalang || 0,
           };
         } else {
           this.listDataPackageMalang = {
@@ -157,6 +176,12 @@ export class HomeComponent implements OnInit {
           };
 
           this.listDataPackageNoTakeMalang = {
+            progress: 0,
+            totalProgress: 0,
+            percentage: 0,
+          };
+
+          this.listDataPackageTakeMalang = {
             progress: 0,
             totalProgress: 0,
             percentage: 0,
@@ -165,8 +190,8 @@ export class HomeComponent implements OnInit {
 
         if (this.getDataPackageSurabaya) {
           // Progress Paket Surabaya
-          const paketProgressSurabaya = parseInt(this.getDataPackageSurabaya?.count_progress) ?? 0;
-          const totalPaketProgressSurabaya = parseInt(this.getDataPackageSurabaya?.count_completed) ?? 0;
+          const paketProgressSurabaya = parseInt(this.getDataPackageSurabaya?.count_progress) || 0;
+          const totalPaketProgressSurabaya = parseInt(this.getDataPackageSurabaya?.count_completed) || 0;
           this.progressPercentagePackageSurabaya =
             paketProgressSurabaya || totalPaketProgressSurabaya === 0
               ? 0
@@ -174,12 +199,12 @@ export class HomeComponent implements OnInit {
           this.listDataPackageSurabaya = {
             progress: paketProgressSurabaya,
             totalProgress: totalPaketProgressSurabaya,
-            percentage: this.progressPercentagePackageSurabaya ?? 0,
+            percentage: this.progressPercentagePackageSurabaya || 0,
           };
 
           // Progress Paket Sudah Kirim Surabaya
-          const paketProgressSendSurabaya = parseInt(this.getDataPackageSurabaya?.count_delivery) ?? 0;
-          const totalPaketProgressSendSurabaya = parseInt(this.getDataPackageSurabaya?.count_completed) ?? 0;
+          const paketProgressSendSurabaya = parseInt(this.getDataPackageSurabaya?.count_delivery) || 0;
+          const totalPaketProgressSendSurabaya = parseInt(this.getDataPackageSurabaya?.count_completed) || 0;
           this.progressPercentagePackageSendSurabaya =
             paketProgressSendSurabaya || totalPaketProgressSendSurabaya === 0
               ? 0
@@ -187,12 +212,12 @@ export class HomeComponent implements OnInit {
           this.listDataPackageSendSurabaya = {
             progress: paketProgressSendSurabaya,
             totalProgress: totalPaketProgressSendSurabaya,
-            percentage: this.progressPercentagePackageSendSurabaya ?? 0,
+            percentage: this.progressPercentagePackageSendSurabaya || 0,
           };
 
           // Progress Paket Belum Diambil Surabaya
-          const paketProgressNoTakeSurabaya = parseInt(this.getDataPackageSurabaya?.count_nocompleted) ?? 0;
-          const totalPaketProgressNoTakeSurabaya = parseInt(this.getDataPackageSurabaya?.count_completed) ?? 0;
+          const paketProgressNoTakeSurabaya = parseInt(this.getDataPackageSurabaya?.count_nocompleted) || 0;
+          const totalPaketProgressNoTakeSurabaya = parseInt(this.getDataPackageSurabaya?.count_completed) || 0;
           this.progressPercentagePackageNoTakeSurabaya =
             paketProgressNoTakeSurabaya || totalPaketProgressNoTakeSurabaya === 0
               ? 0
@@ -200,7 +225,20 @@ export class HomeComponent implements OnInit {
           this.listDataPackageNoTakeSurabaya = {
             progress: paketProgressNoTakeSurabaya,
             totalProgress: totalPaketProgressNoTakeSurabaya,
-            percentage: this.progressPercentagePackageNoTakeSurabaya ?? 0,
+            percentage: this.progressPercentagePackageNoTakeSurabaya || 0,
+          };
+
+          // Progress Paket Sudah Diambil Surabaya
+          const paketProgressTakeSurabaya = parseInt(this.getDataPackageSurabaya?.count_completed) || 0;
+          const totalPaketProgressTakeSurabaya = parseInt(this.getDataPackageSurabaya?.count_completed) || 0;
+          this.progressPercentagePackageNoTakeSurabaya =
+            paketProgressTakeSurabaya || totalPaketProgressTakeSurabaya === 0
+              ? 0
+              : (totalPaketProgressTakeSurabaya / paketProgressTakeSurabaya) * 100;
+          this.listDataPackageTakeSurabaya = {
+            progress: paketProgressTakeSurabaya,
+            totalProgress: totalPaketProgressTakeSurabaya,
+            percentage: this.progressPercentagePackageTakeSurabaya || 0,
           };
         } else {
           this.listDataPackageSurabaya = {
@@ -220,12 +258,18 @@ export class HomeComponent implements OnInit {
             totalProgress: 0,
             percentage: 0,
           };
+
+          this.listDataPackageTakeSurabaya = {
+            progress: 0,
+            totalProgress: 0,
+            percentage: 0,
+          };
         }
 
         if (this.getDataPassengerMalang) {
           // Progress Passenger Package Malang Have Data
-          const paketPassengerMalang = parseInt(this.getDataPassengerMalang?.count_status) ?? 0;
-          const totalPaketPassengerMalang = parseInt(this.getDataPassengerMalang?.count_status_completed) ?? 0;
+          const paketPassengerMalang = parseInt(this.getDataPassengerMalang?.count_status) || 0;
+          const totalPaketPassengerMalang = parseInt(this.getDataPassengerMalang?.count_status_completed) || 0;
           this.progressPercentagePackagePassengerMalang =
             paketPassengerMalang || totalPaketPassengerMalang === 0
               ? 0
@@ -233,12 +277,12 @@ export class HomeComponent implements OnInit {
           this.getDataPackagePassengerMalang = {
             progress: paketPassengerMalang,
             totalProgress: totalPaketPassengerMalang,
-            percentage: this.progressPercentagePackagePassengerMalang ?? 0,
+            percentage: this.progressPercentagePackagePassengerMalang || 0,
           };
 
           // Progress Passenger Malang Have Data
-          const penumpangProgressMalang = parseInt(this.getDataPassengerMalang?.count_progress) ?? 0;
-          const totalPenumpangProgressMalang = parseInt(this.getDataPassengerMalang?.count_completed) ?? 0;
+          const penumpangProgressMalang = parseInt(this.getDataPassengerMalang?.count_progress) || 0;
+          const totalPenumpangProgressMalang = parseInt(this.getDataPassengerMalang?.count_completed) || 0;
           this.progressPercentagePassengerMalang =
             penumpangProgressMalang || totalPenumpangProgressMalang === 0
               ? 0
@@ -246,7 +290,7 @@ export class HomeComponent implements OnInit {
           this.listDataPassengerMalang = {
             progress: penumpangProgressMalang,
             totalProgress: totalPenumpangProgressMalang,
-            percentage: this.progressPercentagePassengerMalang ?? 0,
+            percentage: this.progressPercentagePassengerMalang || 0,
           };
         } else {
           // Progress Passenger Package Malang No Data
@@ -266,8 +310,8 @@ export class HomeComponent implements OnInit {
 
         if (this.getDataPassengerSurabaya) {
           // Progress Passenger Package Surabaya Have Data
-          const paketPassengerSurabaya = parseInt(this.getDataPassengerSurabaya?.count_status) ?? 0;
-          const totalPaketPassengerSurabaya = parseInt(this.getDataPassengerSurabaya?.count_status_completed) ?? 0;
+          const paketPassengerSurabaya = parseInt(this.getDataPassengerSurabaya?.count_status) || 0;
+          const totalPaketPassengerSurabaya = parseInt(this.getDataPassengerSurabaya?.count_status_completed) || 0;
           this.progressPercentagePackagePassengerSurabaya =
             paketPassengerSurabaya || totalPaketPassengerSurabaya === 0
               ? 0
@@ -275,12 +319,12 @@ export class HomeComponent implements OnInit {
           this.getDataPackagePassengerSurabaya = {
             progress: paketPassengerSurabaya,
             totalProgress: totalPaketPassengerSurabaya,
-            percentage: this.progressPercentagePackagePassengerSurabaya ?? 0,
+            percentage: this.progressPercentagePackagePassengerSurabaya || 0,
           };
 
           // Progress Passenger Surabaya Have Data
-          const penumpangProgressSurabaya = parseInt(this.getDataPassengerSurabaya?.count_progress) ?? 0;
-          const totalPenumpangProgressSurabaya = parseInt(this.getDataPassengerSurabaya?.count_completed) ?? 0;
+          const penumpangProgressSurabaya = parseInt(this.getDataPassengerSurabaya?.count_progress) || 0;
+          const totalPenumpangProgressSurabaya = parseInt(this.getDataPassengerSurabaya?.count_completed) || 0;
           this.progressPercentagePassengerSurabaya =
             penumpangProgressSurabaya || totalPenumpangProgressSurabaya === 0
               ? 0
@@ -288,7 +332,7 @@ export class HomeComponent implements OnInit {
           this.listDataPassengerSurabaya = {
             progress: penumpangProgressSurabaya,
             totalProgress: totalPenumpangProgressSurabaya,
-            percentage: this.progressPercentagePassengerSurabaya ?? 0,
+            percentage: this.progressPercentagePassengerSurabaya || 0,
           };
         } else {
           // Progress Passenger Package Surabaya No Data
